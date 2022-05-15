ea426d252f30abf8deb349b9d311550a1220b0f5
Rama  Labban <rama2s2labban@gmail.com>
2022-05-15 20:31:33 +0300

    edit menu category landing page

--- src/shared/services/cart.service.ts
+++ src/shared/services/cart.service.ts
@@ -1,20 +1,36 @@
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  cartItems: any = []
+  private dataSource: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  data: Observable<any> = this.dataSource.asObservable();
+
+  cartItems: any = [];
+  total_price: number = 0;
   constructor() { }
 
-  addToCart(item: any) {
-    console.log(item)
+  addToCart(item: any, obj: any, operation?:string) { // obj :  is the complete object
+    console.log("From service : ",obj)
     const menuItem = this.cartItems.findIndex(x => x.id === item.id)
-    if (menuItem == -1)
-      this.cartItems.push({ id: item.id, name: item.name, quantity: 1, price:item.price })
-    else
+    if (menuItem == -1){
+    console.log("service: obj. : ", { id: item.id, name: item.name, quantity: 1, price: item.price, optionIds: obj.optionIds})
+      this.cartItems.push({ id: item.id, name: item.name, quantity: 1, price: item.price, optionIds: obj.optionIds})
+     }
+     else if(operation =="minus"){
+      this.cartItems[menuItem].quantity -= 1;
+
+    }
+      else {
       this.cartItems[menuItem].quantity += 1;
+
+    }
+    this.calculateTotalPrice();
+    console.log("From service  this.total_price: ", this.total_price)
+    localStorage.setItem('total_price', JSON.stringify( this.total_price));
+    this.sendData(this.total_price);
     localStorage.setItem('cart', JSON.stringify(this.cartItems))
   }
 
@@ -23,7 +39,30 @@ export class CartService {
     return of(this.cartItems)
   }
 
+  getTotalPrice(): Observable<any>{
+    this.total_price = JSON.parse(localStorage.getItem('total_price') || '0')
+    return of(this.total_price)
+  }
+
   deleteCart() {
-    localStorage.setItem('cart', JSON.stringify([]))
+    localStorage.setItem('cart', JSON.stringify([]));
+    localStorage.setItem('total_price', JSON.stringify(0));
+  }
+
+  calculateTotalPrice(){
+    this.total_price = 0;
+   let totalOptions = 0;
+
+  this.cartItems.forEach(
+    el => {
+      el.optionIds.forEach( e =>{
+      totalOptions += e.addtionalPrice;
+    })
+     this.total_price +=  el.price * el.quantity + totalOptions;
+    }
+  );
+  }
+  sendData(data: any) {
+    this.dataSource.next(data);
   }
 }
