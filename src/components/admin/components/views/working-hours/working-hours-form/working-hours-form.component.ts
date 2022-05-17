06be8bbe0764e0ba02cf224adbbb0943d94a5f1f
Rama  Labban <rama2s2labban@gmail.com>
2022-05-17 16:27:53 +0300

    form working hours

--- src/components/admin/components/views/working-hours/working-hours-form/working-hours-form.component.ts
+++ src/components/admin/components/views/working-hours/working-hours-form/working-hours-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkingHoursService } from '../working-hours.service';
 
 @Component({
   selector: 'app-working-hours-form',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./working-hours-form.component.scss']
 })
 export class WorkingHoursFormComponent implements OnInit {
+  form$: any;
+  type: string = 'add';
 
-  constructor() { }
+  constructor(
+    private _workingHoursService: WorkingHoursService
+  ) { }
 
   ngOnInit(): void {
+    this.getWorkingHoursForm();
   }
-
+  getWorkingHoursForm() {
+    this._workingHoursService.getWorkingHoursForm().subscribe(items => {
+      this.form$ = items
+    })
+  }
 }
