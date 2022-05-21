a96e92ef2e31969075203b8df8b18fc175864212
Rama  Labban <rama2s2labban@gmail.com>
2022-05-21 16:02:16 +0300

    kitchen page

---
+++ src/components/admin/components/views/kitchen/kitchen-orders/kitchen-orders.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KitchenOrdersComponent } from './kitchen-orders.component';
+
+describe('KitchenOrdersComponent', () => {
+  let component: KitchenOrdersComponent;
+  let fixture: ComponentFixture<KitchenOrdersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ KitchenOrdersComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(KitchenOrdersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
